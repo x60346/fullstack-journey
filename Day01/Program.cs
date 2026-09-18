545936c3394c81dd8218444c03c66d6fe85ff3d2
Anita <x60346@datacom.com.tw>
2026-09-18 10:22:27 +0800

    feat: Day01 practice

---
+++ Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] myArray = [1, 3, 3, 2, 3, 1];
+Dictionary<int,int> dict = new Dictionary<int,int>();
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    if (dict.TryGetValue(myArray[i], out int value)) dict[myArray[i]] = value + 1;
+    else dict[myArray[i]] = 1;
+};
+
+int maxNum = myArray[0];
+int[] dictKey = dict.Keys.ToArray();
+for(int i = 1; i < dictKey.Length; i++)
+{
+    if (dict[dictKey[i]] > dict[maxNum]) maxNum = dictKey[i];
+};
+
+Console.WriteLine(maxNum);
+Console.ReadKey();
