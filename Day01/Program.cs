c8095267d69885b4947fa766ebf956418dfd9b99
Anita <x60346@datacom.com.tw>
2026-09-18 11:42:31 +0800

    feat: 週五作業 - 把邏輯抽成一個方法 static int FindMostFrequent(int[] numbers)，處理上面兩個問題 - 在 Main 裡用 if 逐一驗證你的 3 組測試案例，不符合就印出「FAIL」，全過印「PASS」 - 把專案 commit 並 push 到 fullstack-journey repo，貼給我程式碼和 repo 連結

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,18 +1,65 @@
-﻿int[] myArray = [1, 3, 3, 2, 3, 1];
-Dictionary<int,int> dict = new Dictionary<int,int>();
+﻿// 測試
+int[] test1 = [];
+int[] test2 = [1, 1, 2, 2, 3];
+int[] test3 = [7];
+if (FindMostFrequent(test1) == "陣列為空") Console.WriteLine("PASS");
+else Console.WriteLine("FAIL");
+if (FindMostFrequent(test2) == "1 和 2") Console.WriteLine("PASS");
+else Console.WriteLine("FAIL");
+if (FindMostFrequent(test3) == "7") Console.WriteLine("PASS");
+else Console.WriteLine("FAIL");
 
-for(int i = 0; i < myArray.Length; i++)
+// 自由輸入陣列
+List<int> myArray = new List<int>();
+do
 {
-    if (dict.TryGetValue(myArray[i], out int value)) dict[myArray[i]] = value + 1;
-    else dict[myArray[i]] = 1;
-};
+    Console.WriteLine("請輸入整數，若要停止請輸入N：");
+    string? input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input)) Console.WriteLine("輸入不可為空");
+    else if (input.ToUpper() == "N") break;
+    else if (int.TryParse(input, out int result)) myArray.Add(result);
+    else Console.WriteLine("僅可輸入整數");
+}
+while (true);
 
-int maxNum = myArray[0];
-int[] dictKey = dict.Keys.ToArray();
-for(int i = 1; i < dictKey.Length; i++)
+// 跑 func
+string answer;
+answer = FindMostFrequent(myArray.ToArray());
+// 答案輸出
+if (answer != "陣列為空")
 {
-    if (dict[dictKey[i]] > dict[maxNum]) maxNum = dictKey[i];
-};
-
-Console.WriteLine(maxNum);
+    Console.WriteLine($"出現最多次的數字是： {answer}");
+} else
+{
+    Console.WriteLine($"錯誤： {answer}");
+}
 Console.ReadKey();
+
+// 篩出最多次數數字
+static string FindMostFrequent(int[] numbers)
+{
+    if (numbers.Length == 0) return "陣列為空";
+
+    Dictionary<int, int> dict = new Dictionary<int, int>();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (dict.TryGetValue(numbers[i], out int value)) dict[numbers[i]] = value + 1;
+        else dict[numbers[i]] = 1;
+    }
+
+    int[] maxNum = [numbers[0]];
+    int[] dictKey = dict.Keys.ToArray();
+    for (int i = 0; i < dictKey.Length; i++)
+    {
+        if (dict[dictKey[i]] > dict[maxNum[0]]) {
+            System.Array.Resize(ref maxNum, 1);
+            maxNum[0] = dictKey[i];
+        } 
+        else if (dict[dictKey[i]] == dict[maxNum[0]] && dictKey[i] != maxNum[0]) {
+            System.Array.Resize(ref maxNum, maxNum.Length + 1);
+            maxNum[maxNum.Length - 1] = dictKey[i];
+        }
+    }
+    string answer = string.Join(" 和 ",maxNum);
+    return answer;
+}
